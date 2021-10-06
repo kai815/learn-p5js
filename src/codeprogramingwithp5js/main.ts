e858a0d0f4239db34dbd5b50740ce80e5a41ad79
kai815 <h.kaizuka815@gmail.com>
2021-10-06 18:52:58 +0900

    move cricle

--- src/codeprogramingwithp5js/main.ts
+++ src/codeprogramingwithp5js/main.ts
@@ -1,20 +1,21 @@
 import p5 from "p5";
 
 const sketch = (p: p5) => {
+  let circleX = 100;
   p.setup = () => {
     p.createCanvas(400, 300);
-    p.background(0);
   };
 
   p.draw = () => {  
+    p.background(0);
     p.noStroke();
-    p.fill(255,50)
-    p.circle(p.mouseX,p.mouseY,24);
-    // p.background(0);
+    p.fill(255)
+    p.circle(circleX,150,64);
+    circleX = circleX + 1;
   };
 
-  p.mousePressed = ()=>{
-    p.background(0);
+  p.mousePressed = () => {
+    circleX = 0;
   }
 };
 
