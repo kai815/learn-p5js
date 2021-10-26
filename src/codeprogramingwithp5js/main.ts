ef227dc9d80cc419a16ff38b7d5c965f07e1b034
kai815 <h.kaizuka815@gmail.com>
2021-10-26 22:18:59 +0900

    array loop p5

--- src/codeprogramingwithp5js/main.ts
+++ src/codeprogramingwithp5js/main.ts
@@ -1,20 +1,20 @@
 import p5 from "p5";
-import {Bubble} from "./Bubble";
+// import {Bubble} from "./Bubble";
 
 const sketch = (p: p5) => {
-  let bubble1,bubble2;
+  let nums = [100,25,46,72]
   p.setup = () => {
-    p.createCanvas(600, 400);
+    p.createCanvas(500, 400);
   };
 
   p.draw = () => {
-    p.background(50);
-    bubble1 = new Bubble(100,200,40,p);
-    bubble2 = new Bubble(50,150,10,p);
-    bubble1.move();
-    bubble1.show();
-    bubble2.move();
-    bubble2.show();
+    p.background(0);
+
+    for(let i = 0; i < 4; i++){
+      p.stroke(255)
+      p.noFill();
+      p.ellipse(i * 100 + 100,200,nums[i], nums[i])
+    }
   };
   
 };
