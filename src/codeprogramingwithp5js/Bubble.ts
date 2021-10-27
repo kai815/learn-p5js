53cd07f85b2449d5795a46b9fea6a4e438e417fd
kai815 <h.kaizuka815@gmail.com>
2021-10-27 20:16:11 +0900

    learn array and cloud

--- src/codeprogramingwithp5js/Bubble.ts
+++ src/codeprogramingwithp5js/Bubble.ts
@@ -16,9 +16,11 @@ export class Bubble {
     this.y = this.y + this.p.random(-10, 10)
   }
   show(){ 
-    this.p.stroke(255)
-    this.p.strokeWeight(4);
-    this.p.noFill();
+    // this.p.stroke(255)
+    // this.p.strokeWeight(4);
+    // this.p.noFill();
+    this.p.noStroke();
+    this.p.fill(255,20)
     this.p.ellipse(this.x,this.y, this.r * 2)
   }
 }
--- src/codeprogramingwithp5js/main.ts
+++ src/codeprogramingwithp5js/main.ts
@@ -1,22 +1,25 @@
 import p5 from "p5";
-// import {Bubble} from "./Bubble";
+import {Bubble} from "./Bubble";
 
 const sketch = (p: p5) => {
-  let nums = [100,25,46,72]
+  let bubbles:Bubble[]=[]
   p.setup = () => {
-    p.createCanvas(500, 400);
-  };
-
-  p.draw = () => {
-    p.background(0);
-
-    for(let i = 0; i < 4; i++){
-      p.stroke(255)
-      p.noFill();
-      p.ellipse(i * 100 + 100,200,nums[i], nums[i])
+    p.createCanvas(600, 400);
+    for (let i = 0; i < 1000; i++) {
+      let x = p.random(p.width)
+      let y = p.random(p.height)
+      let r = p.random(10,40)
+      bubbles[i]= new Bubble(x,y,r,p)
     }
   };
-  
+  console.log(bubbles);
+  p.draw = () => {
+    p.background(0);
+    for(let i = 0; i < bubbles.length; i++){
+      bubbles[i].move();
+      bubbles[i].show();
+    }
+  };
 };
 
 new p5(sketch);
