35416983e8dd3c96ec4292c8270884b618788c12
kai815 <h.kaizuka815@gmail.com>
2021-11-06 19:15:54 +0900

    色と波の変更

--- src/wave/main.ts
+++ src/wave/main.ts
@@ -5,14 +5,14 @@ const sketch = (p: p5) => {
   p.setup = () => {
     p.createCanvas(600, 600);
     p.noStroke();
-    p.fill(200,10,40);
+    p.fill(0, 100, 255);
   };
 
   p.draw = () => {
-    p.background(100,10);
+    p.background(255,10);
     // make a x and y grid of ellipses
-    for (let x = 0; x <= p.width; x = x + 30) {
-      for (let y = 0; y <= p.height; y = y + 30) {
+    for (let x = 0; x <= p.width; x = x + 10) {
+      for (let y = 0; y <= p.height; y = y + 10) {
         // starting point of each circle depends on mouse position
         let xAngle = p.map(p.mouseX, 0, p.width, -4 * p.PI, 4 * p.PI, true);
         let yAngle = p.map(p.mouseY, 0, p.height, -4 * p.PI, 4 * p.PI, true);
