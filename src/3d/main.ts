96041a09da553e7eb4304434a1830ccde0977dea
kai815 <h.kaizuka815@gmail.com>
2021-11-13 18:19:36 +0900

    平面図系の描画

--- src/3d/main.ts
+++ src/3d/main.ts
@@ -2,12 +2,20 @@ import p5 from "p5";
 
 const sketch = (p: p5) => {
   p.setup = () => {
-    p.createCanvas(400, 400);
+    p.createCanvas(710, 400, p.WEBGL);
   };
 
   p.draw = () => {
-    p.background(220);
-    p.ellipse(50, 50, 80, 80);
+    p.background(250);
+    //平面の描画
+    // p.translate(-240, -100, 0);
+    p.normalMaterial();
+    p.push();
+    p.rotateZ(p.frameCount * 0.01);
+    p.rotateX(p.frameCount * 0.01);
+    p.rotateY(p.frameCount * 0.01);
+    p.plane(200);
+    // p.pop();
   };
 };
 
